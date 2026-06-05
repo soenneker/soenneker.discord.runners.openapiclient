154a9a883e83589f7ba9e855de7d729ffc694ce9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:20 -0500

    xml commenting

--- src/Soenneker.Discord.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Discord.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Discord.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Discord.OpenApiClient";
 }
