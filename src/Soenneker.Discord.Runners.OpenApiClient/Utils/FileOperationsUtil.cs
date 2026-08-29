cab500d09151571395e5414a2132815609f09267
soenneker <jake@soenneker.com>
2026-08-29 14:50:32 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Discord.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Discord.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -21,7 +21,7 @@ using System.Collections.Generic;
 
 namespace Soenneker.Discord.Runners.OpenApiClient.Utils;
 
-///<inheritdoc cref="IFileOperationsUtil"/>
+/// <inheritdoc cref="IFileOperationsUtil"/>
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
